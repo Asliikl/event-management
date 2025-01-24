707a710a4ba7f28706fb202fb9150beb84e178cf
Asliikl <asliikiel9@gmail.com>
2025-01-24 22:17:56 +0300

    feat: Add optional relationship loading, implement universal relationship loading functionality, load participant relationships, and set up authentication using Sanctum

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,41 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-
-
-    public function index()
+    use CanLoadRelationships;
+    private array $relations = ['user', 'attendees', 'attendees.user'];
+    public function __construct()
     {
-        $query=Event::query();
-        $relations =['user','attendees','attendees.user'];
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+   public function index()
+    {
+        $query=$this->loadRelationships(Event::query());
 
-        foreach ($relations as $relation) {
-            $query->when(
-              $this->shouldIncludeRelation($relation),
-              fn($q) => $q->with($relation)
-            );
-        }
         return EventResource::collection(
            $query->latest()->paginate()
         );
-    }
-
-
-    protected function shouldIncludeRelation(string $relation): bool
-    {
-        $include = request()->query('include');
-
-        if (!$include){
-            return false;
-        }
-
-        $relations =array_map('trim',explode(',',$include));
-
-        return in_array($relation,$relations);
     }
 
     public function store(Request $request)
@@ -50,17 +34,18 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time'
             ]),
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]);
 
-        return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
     }
 
     public function show(Event $event)
     {
         $event->load('user','attendees');
-        return new EventResource($event);
-
+        return new EventResource(
+            $this->loadRelationships($event)
+        );
     }
 
     public function update(Request $request, Event $event)
@@ -73,7 +58,7 @@ class EventController extends Controller
                 'end_time' => 'sometimes|date|after:start_time'
             ])
         );
-        return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
     }
 
 
